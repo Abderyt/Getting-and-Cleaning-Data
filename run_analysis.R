61500cfdba6934fafbf6af5d45c8d5886d7b9781
Abderyt <Abderyt@users.noreply.github.com>
2014-06-22 19:40:08 +0200

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,34 @@
+test.x <- read.table("UCI HAR Dataset\\test\\X_test.txt")
+test.y <- read.table("UCI HAR Dataset\\test\\y_test.txt")
+test.sub <- read.table("UCI HAR Dataset\\test\\subject_test.txt")
+
+train.x <- read.table("UCI HAR Dataset\\train\\X_train.txt")
+train.y <- read.table("UCI HAR Dataset\\train\\y_train.txt")
+train.sub <- read.table("UCI HAR Dataset\\train\\subject_train.txt")
+
+lab <- read.table("UCI HAR Dataset\\features.txt", stringsAsFactors=FALSE)
+act.lab <- read.table("UCI HAR Dataset\\activity_labels.txt", stringsAsFactors=FALSE)
+activity.labels <- act.lab[, 2]
+names(activity.labels) <- act.lab[, 1]
+
+w.mean <- grep('mean()', lab[,2], fixed=TRUE)
+w.std <- grep('std()', lab[,2], fixed=TRUE)
+ind <- sort(c(w.mean, w.std))
+
+test.x <- test.x[, ind]
+train.x <- train.x[, ind]
+
+colnames(test.x) <- lab[ind, 2]
+colnames(train.x) <- lab[ind, 2]
+
+test.x$Set <- 'test'
+train.x$Set <- 'training'
+
+test.x$Activity.label <- activity.labels[as.character(test.y[,1])]
+test.x$Subject <- test.sub[,1]
+train.x$Activity.label <- activity.labels[as.character(train.y[,1])]
+train.x$Subject <- train.sub[,1]
+
+whole <- rbind(test.x, train.x)
+
+write.table(whole, 'wholeData.txt')
